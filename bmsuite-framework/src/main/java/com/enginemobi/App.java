c9f85207951dd4a630810d8218ccd1543b0a946c
smaxll <smaxllimit@gmail.com>
2015-09-14 22:10:32 +1000

    create project framework structure

---
+++ bmsuite-framework/src/main/java/com/enginemobi/App.java
@@ -0,0 +1,13 @@
+package com.enginemobi;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
