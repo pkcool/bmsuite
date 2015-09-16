3b91629bf694f49c7b8914a64a171e2aad81e4f7
smaxll <smaxllimit@gmail.com>
2015-09-17 00:51:51 +1000

    Added interface for generic entity

---
+++ bmsuite-framework/src/main/java/com/enginemobi/core/generic/domain/BmSuiteBaseEntity.java
@@ -0,0 +1,11 @@
+package com.enginemobi.core.generic.domain;
+
+import java.io.Serializable;
+
+/**
+ * Created by smaxll on 17/09/2015.
+ */
+public interface BmSuiteBaseEntity<K extends Serializable & Comparable<K>, E extends BmSuiteBaseEntity<K, ?>>
+        extends Serializable, Comparable<E>  {
+
+}
---
+++ bmsuite-framework/src/main/java/com/enginemobi/core/generic/domain/BmSuiteBaseEntityImpl.java
@@ -0,0 +1,7 @@
+package com.enginemobi.core.generic.domain;
+
+/**
+ * Created by smaxll on 17/09/2015.
+ */
+public abstract class BmSuiteBaseEntityImpl implements BmSuiteBaseEntity {
+}
