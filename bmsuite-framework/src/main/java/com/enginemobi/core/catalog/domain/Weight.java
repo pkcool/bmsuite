df5a997e576421283fb6429a228bde406cffa602
smaxll <smaxllimit@gmail.com>
2015-09-17 23:40:19 +1000

    Implemented product weight class

--- bmsuite-framework/src/main/java/com/enginemobi/core/catalog/domain/Weight.java
+++ bmsuite-framework/src/main/java/com/enginemobi/core/catalog/domain/Weight.java
@@ -1,10 +1,65 @@
 package com.enginemobi.core.catalog.domain;
 
+import com.enginemobi.common.util.WeightUnitOfMeasureType;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
 import java.io.Serializable;
+import java.math.BigDecimal;
 
 /**
  * Represents product weight
  */
+@Embeddable
 public class Weight implements Serializable {
+
     private static final long serialVersionUID = 1L;
+
+    @Column(name = "WEIGHT")
+    protected BigDecimal weight;
+
+
+    @Column(name = "WEIGHT_UNIT_OF_MEASURE")
+    protected String weightUnitOfMeasure;
+
+    public WeightUnitOfMeasureType getWeightUnitOfMeasure() {
+        return WeightUnitOfMeasureType.getInstance(weightUnitOfMeasure);
+    }
+
+    public void setWeightUnitOfMeasure(WeightUnitOfMeasureType weightUnitOfMeasure) {
+        if (weightUnitOfMeasure != null) {
+            this.weightUnitOfMeasure = weightUnitOfMeasure.getType();
+        }
+    }
+
+    public BigDecimal getWeight() {
+        return weight;
+    }
+
+    public void setWeight(BigDecimal weight) {
+        this.weight = weight;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        if (!getClass().isAssignableFrom(o.getClass())) return false;
+
+        Weight weight1 = (Weight) o;
+
+        if (weight != null ? !weight.equals(weight1.weight) : weight1.weight != null) return false;
+        if (weightUnitOfMeasure != null ? !weightUnitOfMeasure.equals(weight1.weightUnitOfMeasure) : weight1
+                .weightUnitOfMeasure != null)
+            return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = weight != null ? weight.hashCode() : 0;
+        result = 31 * result + (weightUnitOfMeasure != null ? weightUnitOfMeasure.hashCode() : 0);
+        return result;
+    }
 }
