7ae53b0ec025ff2a7996832422779e22e2736a18
pkcool <pkcool@pkcools-MacBook-Pro.local>
2015-09-18 12:42:41 +1000

    Add store interface and the implementation

---
+++ bmsuite-framework/src/main/java/com/enginemobi/core/store/domain/Store.java
@@ -0,0 +1,58 @@
+package com.enginemobi.core.store.domain;
+
+import com.enginemobi.common.persistence.Status;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * Represent merchant store
+ */
+public interface Store extends Status, Serializable {
+
+     Long getId();
+     void setId(Long id);
+
+     String getCode();
+     void setCode(String code);
+
+     String getName();
+     void setName(String name);
+
+     // store address
+     String getAddress1();
+     void setAddress1(String address1);
+
+     String getAddress2();
+     void setAddress2(String address2);
+
+     String getSuburb();
+     void setSuburb(String suburb);
+
+     void setState(String state);
+     String getState();
+
+     String getPostcode();
+     void setPostcode(String postcode);
+
+     String getCountry();
+     void setCountry(String country);
+
+     String getPhone();
+     void setPhone(String phone);
+
+     String getFax();
+     void setFax(String fax);
+
+     String getEmail();
+     void setEmail(String email);
+
+     String getWebsiteUrl();
+     void setWebsiteUrl(String websiteUrl);
+
+     Date getInBusinessSince();
+     void setInBusinessSince(Date inBusinessSince);
+
+
+
+}
---
+++ bmsuite-framework/src/main/java/com/enginemobi/core/store/domain/StoreImpl.java
@@ -0,0 +1,208 @@
+package com.enginemobi.core.store.domain;
+
+import com.enginemobi.common.persistence.ArchiveStatus;
+import com.enginemobi.core.constants.SchemaConstant;
+import org.hibernate.validator.constraints.NotEmpty;
+
+import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.validation.constraints.Pattern;
+import java.util.Date;
+
+@Entity
+@Table(name = "MERCHANT_STORE", schema= SchemaConstant.BMSUITEDB_SCHEMA)
+public class StoreImpl implements Store {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @Column(name = "STORE_ID", unique = true, nullable = false)
+    @TableGenerator(name = SchemaConstant.TABLE_GENERATOR_NAME, table = SchemaConstant.TABLE_GENERATOR_TABLE_NAME,
+            pkColumnName = SchemaConstant.TABLE_GENERATOR_PKCOLUMN_NAME, valueColumnName = SchemaConstant.TABLE_GENERATOR_VALUE_COLUMN_NAME,
+            pkColumnValue = "STORE_SEQ_NEXT_VAL")
+    @GeneratedValue(strategy = GenerationType.TABLE, generator = SchemaConstant.TABLE_GENERATOR_NAME)
+    private Long id;
+
+    @Column(name = "STORE_NAME", nullable = false)
+    private String name;
+
+    @NotEmpty
+    @Pattern(regexp="^[a-zA-Z0-9_]*$")
+    @Column(name = "STORE_CODE", nullable=false, unique=true, length=100)
+    private String code;
+
+    @Column(name = "STORE_FAX")
+    private String fax;
+
+    @Column(name = "STORE_EMAIL")
+    private String email;
+
+    @Column(name = "STORE_WEBSITE_URL")
+    private String websiteUrl;
+
+    @Column(name = "ADDRESS_1")
+    private String address1;
+
+    @Column(name = "ADDRESS_2")
+    private String address2;
+
+    @Column(name = "STORE_SUBURB")
+    private String suburb;
+
+    @Column(name = "STORE_STATE")
+    private String state;
+
+    @Column(name = "STORE_POSTCODE")
+    private String postcode;
+
+    @Column(name = "STORE_COUNTRY")
+    private String country;
+
+    @Column(name = "STORE_PHONE", length=50)
+    private String phone;
+
+    @Temporal(TemporalType.DATE)
+    @Column(name = "IN_BUSINESS_SINCE")
+    private Date inBusinessSince = new Date();
+
+
+    @Embedded
+    protected ArchiveStatus archiveStatus = new ArchiveStatus();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress1() {
+        return address1;
+    }
+
+    public void setAddress1(String address1) {
+        this.address1 = address1;
+    }
+
+    public String getAddress2() {
+        return address2;
+    }
+
+    public void setAddress2(String address2) {
+        this.address2 = address2;
+    }
+
+    public String getSuburb() {
+        return suburb;
+    }
+
+    public void setSuburb(String suburb) {
+        this.suburb = suburb;
+    }
+
+    public String getPostcode() {
+        return postcode;
+    }
+
+    public void setPostcode(String postcode) {
+        this.postcode = postcode;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getFax() {
+        return fax;
+    }
+
+    public void setFax(String fax) {
+        this.fax = fax;
+
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getWebsiteUrl() {
+        return websiteUrl;
+    }
+
+    public void setWebsiteUrl(String websiteUrl) {
+        this.websiteUrl = websiteUrl;
+    }
+
+    public Date getInBusinessSince() {
+        return inBusinessSince;
+    }
+
+    public void setInBusinessSince(Date inBusinessSince) {
+        this.inBusinessSince = inBusinessSince;
+
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public Character getArchived() {
+        ArchiveStatus temp;
+        if (archiveStatus == null) {
+            temp = new ArchiveStatus();
+        } else {
+            temp = archiveStatus;
+        }
+        return temp.getArchived();
+    }
+
+    public void setArchived(Character archived) {
+        if (archiveStatus == null) {
+            archiveStatus = new ArchiveStatus();
+        }
+        archiveStatus.setArchived(archived);
+    }
+
+    public boolean isActive() {
+        return 'Y'!=getArchived();
+    }
+
+}
