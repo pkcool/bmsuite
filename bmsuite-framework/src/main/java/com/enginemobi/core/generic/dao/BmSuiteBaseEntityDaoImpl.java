e5dcb7addc0b353495e3c8089c945ea5c90e0446
pkcool <pkcool@pkcools-MacBook-Pro.local>
2015-09-18 16:46:09 +1000

    Added base entity DAO implementation

---
+++ bmsuite-framework/src/main/java/com/enginemobi/core/generic/dao/BmSuiteBaseEntityDaoImpl.java
@@ -0,0 +1,101 @@
+package com.enginemobi.core.generic.dao;
+
+import com.enginemobi.core.generic.domain.BmSuiteBaseEntity;
+import com.enginemobi.core.generic.util.GenericEntityUtils;
+
+import javax.persistence.EntityManager;
+import javax.persistence.criteria.Expression;
+import javax.persistence.criteria.Order;
+import javax.persistence.metamodel.SingularAttribute;
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * @param <E> entity type
+ */
+public abstract class BmSuiteBaseEntityDaoImpl<K extends Serializable & Comparable<K>, E extends BmSuiteBaseEntity<K, ?>>
+        extends BmSuiteJpaDaoSupport
+        implements BmSuiteBaseEntityDao<K, E> {
+
+    private Class<E> objectClass;
+
+    @SuppressWarnings("unchecked")
+    public BmSuiteBaseEntityDaoImpl() {
+        this.objectClass = (Class<E>) GenericEntityUtils.getGenericEntityClassFromComponentDefinition(getClass());
+    }
+
+    protected final Class<E> getObjectClass() {
+        return objectClass;
+    }
+
+
+    public E getEntity(Class<? extends E> clazz, K id) {
+        return super.getEntity(getObjectClass(), id);
+    }
+
+
+    public E getById(K id) {
+        return super.getEntity(getObjectClass(), id);
+    }
+
+
+    public <V> E getByField(SingularAttribute<? super E, V> attribute, V fieldValue) {
+        return super.getByField(getObjectClass(), attribute, fieldValue);
+    }
+
+
+    public void update(E entity) {
+        super.update(entity);
+    }
+
+
+    public void save(E entity) {
+        super.save(entity);
+    }
+
+
+    public void delete(E entity) {
+        super.delete(entity);
+    }
+
+
+    public E refresh(E entity) {
+        return super.refresh(entity);
+    }
+
+
+    public List<E> list() {
+        return super.listEntity(getObjectClass());
+    }
+
+
+    public <V> List<E> listByField(SingularAttribute<? super E, V> attribute, V fieldValue) {
+        return super.listEntityByField(getObjectClass(), attribute, fieldValue);
+    }
+
+
+    public <T extends E> List<T> list(Class<T> objectClass, Expression<Boolean> filter, Integer limit, Integer offset, Order... orders) {
+        return super.listEntity(objectClass, filter, limit, offset, orders);
+    }
+
+
+    public Long count() {
+        return super.countEntity(getObjectClass());
+    }
+
+
+    public <V> Long countByField(SingularAttribute<? super E, V> attribute, V fieldValue) {
+        return super.countEntityByField(getObjectClass(), attribute, fieldValue);
+    }
+
+
+    public Long count(Expression<Boolean> filter) {
+        return super.countEntity(getObjectClass(), filter);
+    }
+
+
+    @Override
+    public EntityManager getEntityManager() {
+        return super.getEntityManager();
+    }
+}
